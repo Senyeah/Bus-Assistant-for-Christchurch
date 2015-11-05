bf49a224324f6074d2b94bb204f469dc128571bd
Jack Greenhill <jcg63@uclive.ac.nz>
2015-11-05 19:24:18 +1300

    Initial Commit

---
+++ Christchurch Bus Info/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Christchurch Bus Info
+//
+//  Created by Jack Greenhill on 5/11/15.
+//  Copyright © 2015 Miyazu App + Web Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
